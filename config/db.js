2519d24cdccc8f059be33263890f7576d351fbd6
wetsik <122281241+wetsik@users.noreply.github.com>
2025-06-01 18:40:11 +0500

    Fix: added SSL for PostgreSQL on Render

--- config/db.js
+++ config/db.js
@@ -7,6 +7,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false // важно для Render
+  }
 });
 
 module.exports = pool;
